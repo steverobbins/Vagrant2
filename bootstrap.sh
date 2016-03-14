c24c519db15e929d5b20851e88b91764ff33550e
Steve Robbins <steve@steverobbins.com>
2016-03-13 17:01:05 -0700

    new PHP7 repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ apt-get -y update
 apt-get -y install \
   python-software-properties
 
-add-apt-repository ppa:ondrej/php-7.0
+add-apt-repository ppa:ondrej/php
 
 apt-get -y update
 
@@ -19,13 +19,16 @@ apt-get -y install \
   mysql-client-5.6 \
   mysql-server-5.6 \
   nginx \
-  php7.0-fpm \
-  php7.0-dev \
-  php7.0-mysql \
   php7.0-curl \
+  php7.0-dev \
+  php7.0-fpm \
   php7.0-gd \
   php7.0-intl \
+  php7.0-mbstring \
+  php7.0-mcrypt \
+  php7.0-mysql \
   php7.0-sqlite3 \
+  php7.0-zip \
   redis-server \
   redis-tools
 
